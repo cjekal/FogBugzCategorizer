ba4958cf31fa09729fb9faa99c2cad89e3f25842
unknown <anu@deathstar.(none)>
2012-06-13 03:10:35 -0400

    CJ - Moved database operations out of GridView file and into Database file.

---
+++ FogBugzCategorizer.Plugins/Categorizer_Database.cs
@@ -0,0 +1,81 @@
+using FogCreek.FogBugz;
+using FogCreek.FogBugz.Plugins.Api;
+using FogCreek.FogBugz.Plugins.Interfaces;
+
+namespace FogBugzCategorizer.Plugins
+{
+	public partial class CategorizerGridView : IPluginDatabase, IPluginBugJoin, IPluginFilterJoin
+	{
+		#region Implementation of IPluginDatabase
+
+		public int DatabaseSchemaVersion()
+		{
+			return 1;
+		}
+
+		public CTable[] DatabaseSchema()
+		{
+			CTable hoursTable = api.Database.NewTable(api.Database.PluginTableName(Tables.HOURS_TABLE));
+			hoursTable.sDesc = "Hours billed to case";
+			hoursTable.AddIntColumn("ixBug", true, 2);
+			hoursTable.AddFloatColumn("Hours", true, 0);
+			hoursTable.AddAutoIncrementPrimaryKey("Id");
+
+			CTable splitTable = api.Database.NewTable(api.Database.PluginTableName(Tables.SPLIT_TABLE));
+			splitTable.sDesc = "Categorization status for case";
+			splitTable.AddIntColumn("ixBug", true, 2);
+			splitTable.AddVarcharColumn("LastEditor", 255, false);
+			splitTable.AddAutoIncrementPrimaryKey("Id");
+
+			CTable hasHoursFilterTable = api.Database.NewTable(api.Database.PluginTableName(Tables.HOURS_FILTER_TABLE));
+			hasHoursFilterTable.sDesc = "Save the filter selection for Has Hours?";
+			hasHoursFilterTable.AddIntColumn("ixFilter", true, 1);
+			hasHoursFilterTable.AddIntColumn("ixPerson", true, 1);
+			hasHoursFilterTable.AddVarcharColumn("HoursFilterType", 50, true, "Unset");
+			hasHoursFilterTable.AddAutoIncrementPrimaryKey("Id");
+
+			CTable hasSplitFilterTable = api.Database.NewTable(api.Database.PluginTableName(Tables.SPLIT_FILTER_TABLE));
+			hasSplitFilterTable.sDesc = "Save the filter selection for Has Split?";
+			hasSplitFilterTable.AddIntColumn("ixFilter", true, 1);
+			hasSplitFilterTable.AddIntColumn("ixPerson", true, 1);
+			hasSplitFilterTable.AddVarcharColumn("SplitFilterType", 50, true, "Unset");
+			hasSplitFilterTable.AddAutoIncrementPrimaryKey("Id");
+
+			return new[] { hoursTable, splitTable, hasHoursFilterTable, hasSplitFilterTable };
+		}
+
+		public void DatabaseUpgradeBefore(int ixVersionFrom, int ixVersionTo, CDatabaseUpgradeApi apiUpgrade)
+		{
+		}
+
+		public void DatabaseUpgradeAfter(int ixVersionFrom, int ixVersionTo, CDatabaseUpgradeApi apiUpgrade)
+		{
+		}
+
+		#endregion
+
+		#region Implementation of IPluginBugJoin
+
+		public string[] BugJoinTables()
+		{
+			return new[] { Tables.HOURS_TABLE, Tables.SPLIT_TABLE };
+		}
+
+		#endregion
+
+		#region Implementation of IPluginFilterJoin
+
+		public string[] FilterJoinTables()
+		{
+			//return null;
+			return new[] { Tables.HOURS_FILTER_TABLE, Tables.SPLIT_FILTER_TABLE };
+		}
+
+		#endregion
+
+		protected virtual string GetPluginTableName(string tableName)
+		{
+			return api.Database.PluginTableName(PLUGIN_ID, tableName);
+		}
+	}
+}
--- FogBugzCategorizer.Plugins/Categorizer_GridView.cs
+++ FogBugzCategorizer.Plugins/Categorizer_GridView.cs
@@ -11,7 +11,7 @@ using FogCreek.FogBugz.UI.Dialog;
 
 namespace FogBugzCategorizer.Plugins
 {
-	public partial class CategorizerGridView : Plugin, IPluginGridColumn, IPluginDatabase, IPluginBugJoin, IPluginFilterDisplay, IPluginFilterOptions, IPluginFilterJoin, IPluginFilterCommit
+	public partial class CategorizerGridView : Plugin, IPluginGridColumn, IPluginFilterDisplay, IPluginFilterOptions, IPluginFilterCommit
     {
 		protected const string PLUGIN_ID = "FogBugzCategorizer@arpc.com";
 
@@ -19,9 +19,6 @@ namespace FogBugzCategorizer.Plugins
 			: base(api)
         {
         }
-
-		protected string preCommitHoursSelection = FilterType.Unset.ToString();
-		protected string preCommitSplitSelection = FilterType.Unset.ToString();
 
         #region IPluginGridColumn Members
 
@@ -98,63 +95,6 @@ namespace FogBugzCategorizer.Plugins
         }
 
         #endregion
-
-		#region Implementation of IPluginDatabase
-
-		public int DatabaseSchemaVersion()
-		{
-			return 1;
-		}
-
-		public CTable[] DatabaseSchema()
-		{
-			CTable hoursTable = api.Database.NewTable(api.Database.PluginTableName(Tables.HOURS_TABLE));
-			hoursTable.sDesc = "Hours billed to case";
-			hoursTable.AddIntColumn("ixBug", true, 2);
-			hoursTable.AddFloatColumn("Hours",true, 0);
-			hoursTable.AddAutoIncrementPrimaryKey("Id");
-
-			CTable splitTable = api.Database.NewTable(api.Database.PluginTableName(Tables.SPLIT_TABLE));
-			splitTable.sDesc = "Categorization status for case";
-			splitTable.AddIntColumn("ixBug", true, 2);
-			splitTable.AddVarcharColumn("LastEditor", 255, false);
-			splitTable.AddAutoIncrementPrimaryKey("Id");
-
-			CTable hasHoursFilterTable = api.Database.NewTable(api.Database.PluginTableName(Tables.HOURS_FILTER_TABLE));
-			hasHoursFilterTable.sDesc = "Save the filter selection for Has Hours?";
-			hasHoursFilterTable.AddIntColumn("ixFilter", true, 1);
-			hasHoursFilterTable.AddIntColumn("ixPerson", true, 1);
-			hasHoursFilterTable.AddVarcharColumn("HoursFilterType", 50, true, "Unset");
-			hasHoursFilterTable.AddAutoIncrementPrimaryKey("Id");
-
-			CTable hasSplitFilterTable = api.Database.NewTable(api.Database.PluginTableName(Tables.SPLIT_FILTER_TABLE));
-			hasSplitFilterTable.sDesc = "Save the filter selection for Has Split?";
-			hasSplitFilterTable.AddIntColumn("ixFilter", true, 1);
-			hasSplitFilterTable.AddIntColumn("ixPerson", true, 1);
-			hasSplitFilterTable.AddVarcharColumn("SplitFilterType", 50, true, "Unset");
-			hasSplitFilterTable.AddAutoIncrementPrimaryKey("Id");
-
-			return new[] { hoursTable, splitTable, hasHoursFilterTable, hasSplitFilterTable };
-		}
-
-		public void DatabaseUpgradeBefore(int ixVersionFrom, int ixVersionTo, CDatabaseUpgradeApi apiUpgrade)
-		{
-		}
-
-		public void DatabaseUpgradeAfter(int ixVersionFrom, int ixVersionTo, CDatabaseUpgradeApi apiUpgrade)
-		{
-		}
-
-		#endregion
-
-		#region Implementation of IPluginBugJoin 
-
-		public string[] BugJoinTables()
-		{
-			return new[] { Tables.HOURS_TABLE, Tables.SPLIT_TABLE };
-		}
-
-		#endregion
 
 		#region Implementation of IPluginFilterDisplay
 
@@ -311,40 +251,10 @@ namespace FogBugzCategorizer.Plugins
 
 		#endregion
 
-		#region Implementation of IPluginFilterJoin
-
-		public string[] FilterJoinTables()
-		{
-			//return null;
-			return new[] {Tables.HOURS_FILTER_TABLE, Tables.SPLIT_FILTER_TABLE};
-		}
-
-		#endregion
-
-		protected virtual string GetPluginTableName(string tableName)
-		{
-			return api.Database.PluginTableName(PLUGIN_ID, tableName);
-		}
-
-		protected virtual FilterType GetHoursFilterType(CFilter filter)
-		{
-			if (filter.GetPluginField(PLUGIN_ID, "HoursFilterType") != null)
-			{
-				return (FilterType)Enum.Parse(typeof(FilterType), Convert.ToString(filter.GetPluginField(PLUGIN_ID, "HoursFilterType")));
-			}
-			return FilterType.Unset;
-		}
-
-		protected virtual FilterType GetSplitFilterType(CFilter filter)
-		{
-			if (filter.GetPluginField(PLUGIN_ID, "SplitFilterType") != null)
-			{
-				return (FilterType)Enum.Parse(typeof(FilterType), Convert.ToString(filter.GetPluginField(PLUGIN_ID, "SplitFilterType")));
-			}
-			return FilterType.Unset;
-		}
-
 		#region Implementation of IPluginFilterCommit
+
+		protected string preCommitHoursSelection = FilterType.Unset.ToString();
+		protected string preCommitSplitSelection = FilterType.Unset.ToString();
 
 		public bool FilterCommitBefore(CFilter filter)
 		{
@@ -399,5 +309,23 @@ namespace FogBugzCategorizer.Plugins
 		}
 
 		#endregion
-    }
+    
+		protected virtual FilterType GetHoursFilterType(CFilter filter)
+		{
+			if (filter.GetPluginField(PLUGIN_ID, "HoursFilterType") != null)
+			{
+				return (FilterType)Enum.Parse(typeof(FilterType), Convert.ToString(filter.GetPluginField(PLUGIN_ID, "HoursFilterType")));
+			}
+			return FilterType.Unset;
+		}
+
+		protected virtual FilterType GetSplitFilterType(CFilter filter)
+		{
+			if (filter.GetPluginField(PLUGIN_ID, "SplitFilterType") != null)
+			{
+				return (FilterType)Enum.Parse(typeof(FilterType), Convert.ToString(filter.GetPluginField(PLUGIN_ID, "SplitFilterType")));
+			}
+			return FilterType.Unset;
+		}
+	}
 }
